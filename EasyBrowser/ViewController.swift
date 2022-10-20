55886a4fc83d1e383e7e25d9c6288ad6de6dc1da
Andrés Raigoza <81978983+andresr774@users.noreply.github.com>
2022-10-20 11:41:24 -0500

    Initial Commit

---
+++ EasyBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyBrowser
+//
+//  Created by Andres camilo Raigoza misas on 20/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
